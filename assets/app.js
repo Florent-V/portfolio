8f24fccbb2736c8add2bb3a146fcb90e027629d4
Florent-V <vasseurflorent@gmail.com>
2025-06-23 16:31:45 +0200

    init

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-// import { Workbox } from 'workbox-window'
+import { Workbox } from 'workbox-window'
 import { registerVueControllerComponents } from '@symfony/ux-vue'
 import './bootstrap.js'
 /*
@@ -14,27 +14,27 @@ import './styles/app.css'
 registerVueControllerComponents(
   require.context('./vue/controllers', true, /\.vue$/),
 )
-//
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     const wb = new Workbox('/service-worker.js') // Adjusted path
-//
-//     wb.addEventListener('installed', (event) => {
-//       if (event.isUpdate) {
-//         // User experience for updates:
-//         // If there's an update, you might want to show a notification
-//         // or prompt the user to refresh the page.
-//         console.log('New content is available; please refresh.')
-//         // Example: if (confirm('New content is available. Refresh?')) { window.location.reload(); }
-//       }
-//     })
-//
-//     wb.register()
-//       .then((registration) => {
-//         console.log('Service Worker registered with scope:', registration.scope)
-//       })
-//       .catch((error) => {
-//         console.error('Service Worker registration failed:', error)
-//       })
-//   })
-// }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    const wb = new Workbox('/service-worker.js') // Adjusted path
+
+    wb.addEventListener('installed', (event) => {
+      if (event.isUpdate) {
+        // User experience for updates:
+        // If there's an update, you might want to show a notification
+        // or prompt the user to refresh the page.
+        console.log('New content is available; please refresh.')
+        // Example: if (confirm('New content is available. Refresh?')) { window.location.reload(); }
+      }
+    })
+
+    wb.register()
+      .then((registration) => {
+        console.log('Service Worker registered with scope:', registration.scope)
+      })
+      .catch((error) => {
+        console.error('Service Worker registration failed:', error)
+      })
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -41,40 +41,6 @@ Encore
   // but, you probably want this, unless you're building a single-page app
   .enableSingleRuntimeChunk()
 
-  .addPlugin(
-    new GenerateSW({
-      swDest: path.resolve(__dirname, 'public/service-worker.js'),
-      clientsClaim: true,
-      skipWaiting: true,
-      sourcemap: false,
-      // Définir les routes à mettre en cache
-      runtimeCaching: [
-        {
-          urlPattern: /\.(?:js|css)$/,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'static-resources',
-          },
-        },
-        {
-          // Cache les fichiers statiques
-          urlPattern: /\.(?:png|jpg|jpeg|svg|gif|woff|woff2|eot|ttf|otf)$/,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'images',
-          },
-        },
-        {
-          urlPattern: /^\/.*/, // pour toutes les pages Symfony
-          handler: 'NetworkFirst',
-          options: {
-            cacheName: 'pages',
-          },
-        },
-      ],
-    }),
-  )
-
   // Copier les fichiers nécessaires pour la PWA
   .addPlugin(
     new CopyWebpackPlugin({
@@ -137,6 +103,41 @@ Encore
   .enablePostCssLoader()
   // enables Sass/SCSS support
   .enableSassLoader()
+
+// Register the service worker plugin only in production
+if (Encore.isProduction()) {
+  Encore.addPlugin(
+    new GenerateSW({
+      swDest: path.resolve(__dirname, 'public/service-worker.js'),
+      clientsClaim: true,
+      skipWaiting: true,
+      sourcemap: false,
+      runtimeCaching: [
+        {
+          urlPattern: /\.(?:js|css)$/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'static-resources',
+          },
+        },
+        {
+          urlPattern: /\.(?:png|jpg|jpeg|svg|gif|woff|woff2|eot|ttf|otf)$/,
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images',
+          },
+        },
+        {
+          urlPattern: /^\/.*/, // pour toutes les pages Symfony
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'pages',
+          },
+        },
+      ],
+    }),
+  )
+}
 
 // uncomment if you use TypeScript
 //.enableTypeScriptLoader()
