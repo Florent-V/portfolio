ce4ea0c8ae3fc02aac7c117d93ec32afd101b0c7
Florent-V <vasseurflorent@gmail.com>
2025-06-25 12:32:32 +0200

    add subscriber to change locale and to redirect admin to easyadmin panel

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Sets the locale on the request based on the session.
+ */
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private string $defaultLocale;
+
+    public function __construct(string $defaultLocale = 'fr')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        // Try to see if the locale has been set as a _locale routing parameter
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            // If no explicit locale has been set on this request, use one from the session
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            // Must be registered before (i.e. with a higher priority than) the default Locale listener
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
---
+++ src/EventSubscriber/LoginRedirectSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
+
+class LoginRedirectSubscriber implements EventSubscriberInterface
+{
+    private UrlGeneratorInterface $urlGenerator;
+    private AuthorizationCheckerInterface $authorizationChecker;
+
+    public function __construct(UrlGeneratorInterface $urlGenerator, AuthorizationCheckerInterface $authorizationChecker)
+    {
+        $this->urlGenerator = $urlGenerator;
+        $this->authorizationChecker = $authorizationChecker;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            LoginSuccessEvent::class => 'onLoginSuccess',
+        ];
+    }
+
+    public function onLoginSuccess(LoginSuccessEvent $event): void
+    {
+        if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+            $response = new RedirectResponse($this->urlGenerator->generate('admin'));
+        } else {
+            $response = new RedirectResponse($this->urlGenerator->generate('app_home'));
+        }
+
+        $event->setResponse($response);
+    }
+}
