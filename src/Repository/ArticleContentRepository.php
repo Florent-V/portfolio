92fa5947e1e3febda55d3b707dc5d857f7f7b851
Florent-V <vasseurflorent@gmail.com>
2026-05-28 17:58:09 +0200

    fix type for code quality

--- src/Repository/ArticleContentRepository.php
+++ src/Repository/ArticleContentRepository.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repository;
 
 use App\Entity\ArticleContent;
@@ -8,11 +10,6 @@ use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<ArticleContent>
- *
- * @method ArticleContent|null find($id, $lockMode = null, $lockVersion = null)
- * @method ArticleContent|null findOneBy(array $criteria, array $orderBy = null)
- * @method ArticleContent[]    findAll()
- * @method ArticleContent[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class ArticleContentRepository extends ServiceEntityRepository
 {
